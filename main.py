02b42f1800b3a6a85f1694078e89678c6751fa03
lennysgarage <lennysgarage1@gmail.com>
2020-07-29 17:44:34 -0400

    Updated main page and added backend links for further pages

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/about")
+@app.route("/about/")
 def about():
     return render_template("about.html")
+
+@app.route("/experience/")
+def experience():
+    return render_template("experience.html")
+
+@app.route("/projects/")
+def projects():
+    return render_template("projects.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
